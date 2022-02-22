baca44b3c39e962cc467581b2473fba2f2ec4854
Sai Sankeerth <sanpj2292@github.com>
2022-02-22 13:25:26 +0530

    basic changes to the test utility inside transformer

---
+++ __tests__/utilities/test-utils.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const _ = require('lodash');
+const path = require("path");
+
+function formTestParams(dest, transformAt) {
+	const version = "v0";
+  
+  const transformer = require(
+    path.resolve(__dirname + `../../../${version}/destinations/${dest}/transform`)
+  );
+  
+  //for router test
+  let trCat = '';
+  if (transformAt === 'router') {
+    trCat += 'router_'
+  }
+  const inputDataFile = fs.readFileSync(
+    path.resolve(__dirname, `../data/${dest}_${trCat}input.json`)
+  );
+  const outputDataFile = fs.readFileSync(
+    path.resolve(__dirname, `../data/${dest}_${trCat}output.json`)
+  );
+  const inputData = JSON.parse(inputDataFile);
+  const expected = JSON.parse(outputDataFile);
+	// const cdkDest = cdkDestinations.filter(({ name }) => name === dest)[0];
+	return {
+		input: inputData,
+		expected,
+		transformer,
+		iscdkDest: false
+	};
+}
+
+function executeTransformationTest(dest, transformAt) {
+  const testParams = formTestParams(dest, transformAt);
+  const { iscdkDest, transformer, expected, input } = testParams;
+  describe(`${dest} Tests`, () => {
+    it(`${dest} - ${transformAt} tests`, () => {
+      let actualData = [];
+			if (!iscdkDest) {
+	      if (transformAt === 'processor') {
+	        actualData = input.map(inp => {
+            try {
+              return transformer.process(inp)
+            } catch (error) {
+              const errResp = {
+                error: error.message
+              };
+              console.log(error)
+              if (error.status) {
+                errResp.statusCode = error.status;
+              }
+              return errResp
+            }
+          })
+	      } else {
+	        actualData = transformer.processRouterDest(input)
+	      }
+			} else {
+        // TODO: execute the test from a util function exposed by CDK
+				// actualData = executeTranformationTest(dest);
+			}
+      expect(actualData).toEqual(expected);
+    });
+  });
+}
+
+module.exports = { executeTransformationTest };
